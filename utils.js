7049a4844f7521b95e21ec19b3ee4bea3baf63b1
DimaCrafter <backup.dimacrafter@gmail.com>
2019-12-08 16:14:08 +0400

    Now using X-Real-IP header in docker

--- utils.js
+++ utils.js
@@ -1,29 +1,41 @@
 const log = require('./log');
 const session = require('./session');
-const getBase = (req, res) => {
-    // Parsing IP
-    const ipRaw = Buffer.from(res.getRemoteAddress()).toString('hex');
+
+function getIP (req, res) {
+    // Parsing
+    let ipRaw = Buffer.from(res.getRemoteAddress()).toString('hex');
+
+    // Using X-Real-IP in docker container (172.18.XXX.XXX -> AC12XXXX)
+    if (ipRaw.slice(24, 28) == 'ac12' && req.headers['x-real-ip']) {
+        ipRaw = req.headers['x-real-ip'];
+        return {
+            type: ~ipRaw.indexOf('.') ? 'ipv4' : 'ipv6',
+            value: ipRaw
+        };
+    }
+
+    // Formatting
     const ipParts = [];
     for (let i = 0; i < ipRaw.length; i += 4) ipParts.push(ipRaw.slice(i, i + 4));
-
-    let address;
     if (ipParts[ipParts.length - 3] == 'ffff') {
         const ipv4 = [];
         ipv4.push(parseInt(ipParts[ipParts.length - 2].slice(0, 2), 16));
         ipv4.push(parseInt(ipParts[ipParts.length - 2].slice(2), 16));
         ipv4.push(parseInt(ipParts[ipParts.length - 1].slice(0, 2), 16));
         ipv4.push(parseInt(ipParts[ipParts.length - 1].slice(2), 16));
-        address = {
+        return {
             type: 'ipv4',
             value: ipv4.join('.')
         };
     } else {
-        address = {
+        return {
             type: 'ipv6',
             value: ipParts.join(':')
         };
     }
+}
 
+function getBase (req, res) {
     let controllerProxy;
     const ctx = {
         get db () { log.warn('`this.db` is deprecated, use require(\'dc-api-core/DB\') instead'); },
@@ -34,7 +46,7 @@ const getBase = (req, res) => {
             res.writeHeader('Location', url);
             res.end();
         },
-        address,
+        address: getIP(req, res),
 
         set controller (controller) {
             controllerProxy = new Proxy(controller, {
@@ -48,7 +60,7 @@ const getBase = (req, res) => {
     };
 
     return ctx;
-};
+}
 
 module.exports = {
     async getHTTP (req, res) {
