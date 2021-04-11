7bb3e7ba90c9072b3ce4c13fcdf511e0519f2c2a
DimaCrafter <backup.dimacrafter@gmail.com>
2021-04-11 20:04:18 +0400

    Logs coloring refactoring

--- log.js
+++ log.js
@@ -1,33 +1,92 @@
-const COLOR = 30;
-const BG = 40;
-const reset = '\x1B[0m';
-const bold = '\x1B[1m';
-let colors;
-const ansi = (color, isBG = false) => `\x1B[${color + (isBG ? BG : COLOR)}m`;
-if (process.env.COLORTERM) {
-    if (process.env.COLORTERM == 'truecolor' || process.env.COLORTERM == 'x24') {
-        const rgb = (color, isBG = false) => `\x1B[${8 + (isBG ? BG : COLOR)};2;${color[0]};${color[1]};${color[2]}m`;
-        colors = [rgb([0, 192, 255], true), rgb([0, 192, 64], true), rgb([255, 112, 0], true), rgb([224, 0, 0], true), rgb([224, 0, 0])];
-    } else if (~process.env.COLORTERM.indexOf('256color')) {
-        const named = (color, isBG = false) => `\x1B[${8 + (isBG ? BG : COLOR)};5;${color}m`;
-        colors = [named(39, true), named(35, true), named(202, true), named(160, true), named(160)];
+const config = require('./config');
+const LOG_COLORS = {
+    INFO: {
+        ansi: 6,
+        named: 39,
+        rgb: [0, 192, 255]
+    },
+    OK: {
+        ansi: 2,
+        named: 35,
+        rgb: [0, 192, 64]
+    },
+    WARN: {
+        ansi: 3,
+        named: 202,
+        rgb: [255, 112, 0]
+    },
+    ERR: {
+        ansi: 1,
+        named: 160,
+        rgb: [224, 0, 0]
     }
-} else {
-    colors = [ansi(6, true), ansi(2, true), ansi(3, true), ansi(1, true), ansi(1)];
+};
+
+const FG = 30;
+const BG = 40;
+const RESET = '\x1B[0m';
+const BOLD = '\x1B[1m';
+
+const currentTheme = {};
+function buildTheme (pallete) {
+    let parser;
+    switch (pallete) {
+        case 'rgb':
+            parser = (color, offset) => `\x1B[${offset + 8};2;${color[0]};${color[1]};${color[2]}m`;
+            break;
+        case 'named':
+            parser = (color, offset) => `\x1B[${offset + 8};5;${color}m`;
+            break;
+        case 'ansi':
+            parser = (color, offset = FG) => `\x1B[${color + offset}m`;
+            break;
+        default:
+            process.stdout.write('Incorrect color pallete\n');
+            process.exit();
+    }
+
+    for (const type in LOG_COLORS) {
+        currentTheme[type] = parser(LOG_COLORS[type][pallete], BG);
+    }
+
+    currentTheme.ERR_LINE = parser(LOG_COLORS.ERR[pallete], FG);
+    currentTheme.TEXT = parser(({ ansi: 7, named: 255, rgb: [255, 255, 255] })[pallete], FG);
 }
 
-const print = (color, caption, text) => process.stdout.write(`${colors[color] + ansi(7) + bold} ${caption} ${reset} ${text + reset}\n`);
+if (config.colorPallete) {
+    buildTheme(config.colorPallete);
+} else {
+    if (process.env.COLORTERM) {
+        if (process.env.COLORTERM == 'truecolor' || process.env.COLORTERM == 'x24') {
+            config.colorPallete = 'rgb';
+        } else if (~process.env.COLORTERM.indexOf('256color')) {
+            config.colorPallete = 'named';
+        }
+    } else {
+        config.colorPallete = 'ansi';
+    }
+
+    buildTheme(config.colorPallete);
+}
+
+const print = (type, text) => process.stdout.write(`${currentTheme[type] + currentTheme.TEXT + BOLD} ${type} ${RESET} ${text + RESET}\n`);
 module.exports = {
     text: text => process.stdout.write(text + '\n'),
-    info: text => print(0, 'INFO', text),
-    success: text => print(1, 'OK', text),
-    warn: text => print(2, 'WARN', text),
+    info: text => print('INFO', text),
+    success: text => print('OK', text),
+    warn: text => print('WARN', text),
     error (text, err) {
-        print(3, 'ERR', text);
+        print('ERR', text);
         if (err) {
-            if (!(err instanceof Array)) err = err.toString().split('\n');
-            for (const line of err) process.stdout.write(` ${colors[4]}│${reset} ${line}\n`);
-            process.stdout.write(` ${colors[4]}└─${reset}\n`);
+            if (!(err instanceof Array)) {
+                err = err.toString().split('\n');
+            }
+
+            for (const line of err) {
+                process.stdout.write(` ${currentTheme.ERR_LINE}│${RESET} ${line}\n`);
+            }
+
+            process.stdout.write(` ${currentTheme.ERR_LINE}└─${RESET}\n`);
         }
     }
 };
